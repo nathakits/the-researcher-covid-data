312299d04534e8403e5cd47748ebda33383535d1
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-11-27 19:06:47 +0700

    update script paths

--- vaccine/build_national_timeseries.py
+++ vaccine/build_national_timeseries.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
 
-MAIN_URL = "https://raw.githubusercontent.com/wiki/porames/the-researcher-covid-data"
+# MAIN_URL = "https://raw.githubusercontent.com/wiki/porames/the-researcher-covid-data"
+MAIN_URL = "https://raw.githubusercontent.com/nathakits/the-researcher-covid-data/master/dataset/"
 
 
 def json_load(file_path: str) -> dict:
@@ -23,7 +24,7 @@ def calculate_national_sum_today(data: dict) -> pd.DataFrame:
 
 if __name__ == "__main__":
     # Parse today scraped data
-    moh_prompt_data = json_load("../dataset/provincial-vaccination.json")
+    moh_prompt_data = json_load("dataset/vaccination/provincial-vaccination.json")
     print(moh_prompt_data["update_date"])
     today_data = calculate_national_sum_today(moh_prompt_data)
 
@@ -46,10 +47,10 @@ if __name__ == "__main__":
 
     # Save data as json and csv
     vaccination_timeseries.to_json(
-        "../dataset/national-vaccination-timeseries.json",
+        "dataset/vaccination/national-vaccination-timeseries.json",
         orient="records",
         indent=2,
         force_ascii=False,
     )
-    vaccination_timeseries.to_csv("../dataset/national-vaccination-timeseries.csv", index=False)
+    vaccination_timeseries.to_csv("dataset/vaccination/national-vaccination-timeseries.csv", index=False)
     print("Processed National Timeseries")
